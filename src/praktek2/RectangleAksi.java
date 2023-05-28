8421c569d5a17868ea2296fe412dbec5edafcec2
Nurlia Agustina <liaagustina.app@gmail.com>
2023-05-28 13:41:19 +0800

    mengisi nilai atribut

--- src/praktek2/RectangleAksi.java
+++ src/praktek2/RectangleAksi.java
@@ -3,5 +3,7 @@ package praktek2;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle rl = new Rectangle();
+        rl.panjang = 9;
+        rl.lebar =3;
     }
 }
