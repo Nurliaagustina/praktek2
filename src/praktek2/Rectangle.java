3d8b5a0eab56d0170b95b0994b8ab30f1c612738
Nurlia Agustina <liaagustina.app@gmail.com>
2023-05-28 13:34:08 +0800

    membuat atribut dan behavior

--- src/praktek2/Rectangle.java
+++ src/praktek2/Rectangle.java
@@ -1,4 +1,12 @@
 package praktek2;
 public class Rectangle {
-    
+   double panjang;
+   double lebar;
+   
+   void cetakinfo(){
+       system.out.println("====================");
+       system.out.println("panjang :"+panjang);
+       system.out.println("lebar   :"+lebar);
+       system.out.println("====================");
+        }
 }
